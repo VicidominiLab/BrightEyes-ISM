a47cb6f70b2e465a141fda886a7691ffc642be8b
Alessandro Zunino <zuninoalessandro@gmail.com>
2023-01-18 17:23:58 +0100

    Updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,9 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+extensions.append('autoapi.extension')
+
+autoapi_type = 'python'
+autoapi_dirs = ['../src/brighteyes_ism']
