87fdb3bb917ce6ece9a2f99ef0a2c92bf6b9831c
Alessandro Zunino <zuninoalessandro@gmail.com>
2024-07-09 15:47:12 +0200

    Added docstrings

--- src/brighteyes_ism/simulation/simulator.py
+++ src/brighteyes_ism/simulation/simulator.py
@@ -4,7 +4,33 @@ import copy as cp
 
 
 class ImageSimulator:
+    """
+    Optical settings used to calculate the psf
+    Read more at https://pyfocus.readthedocs.io/en/latest/
 
+    Attributes
+    ----------
+    image : np.ndarray
+        image convolved with the psf and corrupted by shot noise
+    clean_image : np.ndarray
+        image convolved with the psf without noise
+    phantom : np.ndarray
+        stucture of the specimen
+    psf : np.ndarray
+        point spread function. The last dimension is the channel.
+    signal : float
+        brightness of the sample (units: photon counts)
+
+    Methods
+    -------
+    blur :
+        Generates the clean image.
+    poisson_noise :
+        Corrupts the clean image with shot noise.
+    forward :
+        Generates the blurred and noisy image.
+
+    """
     def __init__(self, phantom=None, psf=None, signal=1):
         self.image = None
         self.clean_image = None
