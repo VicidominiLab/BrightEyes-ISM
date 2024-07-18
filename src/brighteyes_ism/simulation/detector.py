a8b971386c2c1795c79a8179b089dc5935e25e27
Alessandro Zunino <zuninoalessandro@gmail.com>
2024-07-18 15:16:51 +0200

    Added docstrings

--- src/brighteyes_ism/simulation/detector.py
+++ src/brighteyes_ism/simulation/detector.py
@@ -4,37 +4,106 @@ from skimage.draw import polygon
 
 
 def circle(n, radius):
+    """
+    It calculates a single binary mask with the shape of a circle.
+
+    Parameters
+    ----------
+    n : int
+        number of pixels of the simulation space, assumed squared.
+    radius : float
+      Radius of the circle in pixel units.
+
+    Returns
+    -------
+    circle_pinhole : np.ndarray (n x n)
+        Image of the binary mask.
+    """
+
     x = np.arange(-(n // 2), n // 2 + 1)
 
     xx, yy = np.meshgrid(x, x)
     r = np.sqrt(xx ** 2 + yy ** 2)
 
-    return np.where(r < radius, 1, 0)
+    circle_pinhole = np.where(r < radius, 1, 0)
+
+    return circle_pinhole
 
 
 def square(n, length):
+    """
+    It calculates a single binary mask with the shape of a square.
+
+    Parameters
+    ----------
+    n : int
+        number of pixels of the simulation space, assumed squared.
+    length : float
+      Length of the square in pixel units.
+
+    Returns
+    -------
+    square_pinhole : np.ndarray (n x n)
+        Image of the binary mask.
+    """
+
     x = np.arange(-(n // 2), n // 2 + 1)
 
     xx, yy = np.meshgrid(x, x)
 
     cond = np.logical_and(np.abs(xx) < length / 2, np.abs(yy) < length / 2)
 
-    return np.where(cond, 1, 0)
+    square_pinhole = np.where(cond, 1, 0)
+
+    return square_pinhole
 
 
 def hexagon(n, radius):
+    """
+    It calculates a single binary mask with the shape of a regular hexagon.
+    The radius is the distance from the center to a vertex.
+
+    Parameters
+    ----------
+    n : int
+        number of pixels of the simulation space, assumed squared.
+    radius : float
+      Distance from the center to a vertex of the hexagon in pixel units.
+
+    Returns
+    -------
+    hex_pinhole : np.ndarray (n x n)
+        Image of the binary mask.
+    """
+
     x = np.array([-1, 0, 1, 1, 0, -1]) * radius * np.cos(np.deg2rad(30)) + n // 2
     y = np.array([1, 2, 1, -1, -2, -1]) * radius * np.sin(np.deg2rad(30)) + n // 2
 
-    hex_array = np.zeros((n, n))
+    hex_pinhole = np.zeros((n, n))
 
     idx = polygon(x, y, shape=(n, n))
-    hex_array[idx] = 1
+    hex_pinhole[idx] = 1
 
-    return hex_array
+    return hex_pinhole
 
 
 def rect_grid(n, x):
+    """
+    It calculates the normalized coordinates of a rectangular grid.
+
+    Parameters
+    ----------
+    n : int
+        number of pixels of the simulation space, assumed squared.
+    x : np.ndarray (N)
+      array with normalized coordinates along a single axis.
+
+    Returns
+    -------
+    s : np.ndarray (2 x N**2)
+        Array with rectangular grid coordinates.
+    """
+
     s = np.empty((n, n, 2))
 
     for n_i, i in enumerate(x):
@@ -45,6 +114,22 @@ def rect_grid(n, x):
 
 
 def hex_grid(n, x):
+    """
+    It calculates the normalized coordinates of a hexagonal grid.
+
+    Parameters
+    ----------
+    n : int
+        number of pixels of the simulation space, assumed squared.
+    x : np.ndarray (N)
+      array with normalized coordinates along a single axis.
+
+    Returns
+    -------
+    s : np.ndarray (2 x N**2)
+        Array with hexagonal grid coordinates.
+    """
+
     s = np.empty((n, n, 2))
 
     for n_i, i in enumerate(x):
@@ -55,6 +140,22 @@ def hex_grid(n, x):
 
 
 def det_coords(n, geometry):
+    """
+    It calculates the coordinates of the pinhole centers from a given geometry.
+
+    Parameters
+    ----------
+    n : int
+        number of pixels of the simulation space, assumed squared.
+    geometry : str
+        Detector geometry. Valid choices are 'rect' or 'hex'.
+
+    Returns
+    -------
+    s : np.ndarray (2 x Nch)
+        Array with coordinates of the pinhole centers.
+    """
+
     x = np.arange(-(n // 2), n // 2 + 1)
 
     if geometry == 'rect':
@@ -70,12 +171,34 @@ def det_coords(n, geometry):
 
 
 def pinhole_array(s, nx, mag, pxsize, pxdim, pinhole_shape):
+    """
+    It calculates Nx x Nx x Nch array of binary masks, describing a detector array.
+
+    Parameters
+    ----------
+    s : np.ndarray (2 x Nch)
+        Array with coordinates of the pinhole centers.
+    nx : int
+        number of pixels of the simulation space, assumed squared.
+    mag : float
+        Magnification of the microscope.
+    pxsize : float
+        Size of the scan pixel.
+    pxdim : float
+        Diameter of the individual pinhole of the detector array.
+    pinhole_shape : str
+        Shape of the invidual pinhole. Valid choices are 'square', 'cirle', or 'hexagon'.
+
+    Returns
+    -------
+    detector : np.ndarray (Nx x Nx x Nch)
+        Numpy array of binary masks. Each channel is a pinhole in a different position.
+    """
+
     radius = pxdim / mag / pxsize / 2
     nch = s.shape[1]
 
     img = np.zeros((nx, nx, nch))
-    detector = np.zeros((nx, nx, nch))
-
     c = s + nx // 2
 
     for k in range(s.shape[1]):
@@ -90,6 +213,8 @@ def pinhole_array(s, nx, mag, pxsize, pxdim, pinhole_shape):
     else:
         raise Exception("Pinhole shape not valid. Select 'square', 'cirle', or 'hexagon'.")
 
+    detector = np.zeros((nx, nx, nch))
+
     for k in range(s.shape[1]):
         detector[..., k] = convolve(pinhole, img[..., k], mode='same')
 
@@ -97,6 +222,19 @@ def pinhole_array(s, nx, mag, pxsize, pxdim, pinhole_shape):
 
 
 def custom_detector(grid):
+    """
+    It calculates Nx x Nx x Nch array of binary masks, describing a detector array.
+
+    Parameters
+    ----------
+    grid : GridParameters
+        Object with array detector and simulation space parameters
+    Returns
+    -------
+    detector : np.ndarray (Nx x Nx x Nch)
+        Numpy array of binary masks. Each channel is a pinhole in a different position.
+    """
+
     s = det_coords(grid.N, grid.geometry)
 
     if grid.name == 'airyscan':
